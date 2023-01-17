dab171136c421b8987d906e1a6d238d63e2abe04
AleehSophia <alessia.casado@gmail.com>
2023-01-17 17:23:16 -0300

    spring initial setup

---
+++ src/main/java/opensource/casado/bookshelf/BookshelfApplication.java
@@ -0,0 +1,14 @@
+package opensource.casado.bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class BookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshelfApplication.class, args);
+	}
+
+}
